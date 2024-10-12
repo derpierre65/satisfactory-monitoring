ce2a69ae0fe670d4dfc84c163c371ddb238e7baa
derpierre65 <git@derpierre65.de>
2024-10-12 17:50:48 +0100

    feat: add toggle dark mode button and basic menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,30 +6,36 @@
           flat
           dense
           round
-          icon="menu"
+          icon="fas fa-bars"
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
 
         <q-toolbar-title>
-          Quasar App
+          FicsIt Remote Monitoring
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-icon :name="$q.dark.isActive ? 'fas fa-moon' : 'fas fa-sun'" class="cursor-pointer" @click="toggleDarkMode">
+          <q-tooltip>Toggle Dark Mode</q-tooltip>
+        </q-icon>
       </q-toolbar>
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
+      model-value
       show-if-above
       bordered
+      :width="leftDrawerOpen ? 250 : 50"
     >
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item clickable>
+          <q-item-section side>
+            <q-icon name="fas fa-dashboard" size="16px" />
+          </q-item-section>
+          <q-item-section v-if="leftDrawerOpen">
+            <q-item-label>Dashboard</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -41,14 +47,23 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import useServerStore from 'stores/server.ts';
+import { useQuasar } from 'quasar';
 
 defineOptions({
-  name: 'MainLayout'
+  name: 'MainLayout',
 });
 
+const serverStore = useServerStore();
+const q = useQuasar();
 const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+function toggleDarkMode() {
+  q.dark.toggle();
+  window.localStorage.setItem('light_mode', !q.dark.isActive);
 }
 </script>
