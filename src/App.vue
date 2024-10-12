61950cbbe6d8c73146f3d5e9b53adb5d7aa97f61
derpierre65 <git@derpierre65.de>
2024-10-12 12:15:45 +0100

    feat: load dark/light mode

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,39 @@
 </template>
 
 <script setup lang="ts">
+import { useQuasar } from 'quasar';
+
 defineOptions({
-  name: 'App'
+  name: 'App',
 });
+
+//#region Composable & Prepare
+//#endregion
+
+//#region Data
+//#endregion
+
+//#region Computed
+//#endregion
+
+//#region Watch
+//#endregion
+
+//#region Lifecycle Events
+//#endregion
+
+//#region Methods
+function updateDarkMode() {
+  let lightMode = false;
+  if (window.localStorage) {
+    lightMode = window.localStorage.getItem('light_mode') === 'true';
+  }
+
+  useQuasar().dark.set(!lightMode);
+}
+//#endregion
+
+//#region Created
+updateDarkMode();
+//#endregion
 </script>
