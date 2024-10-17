16015ef29c841c3191510d9cf34023de23b221e0
derpierre65 <git@derpierre65.de>
2024-10-18 00:20:38 +0100

    chore: add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,53 @@
+import {StorybookConfig} from '@storybook/vue3-vite';
+import vue from '@vitejs/plugin-vue';
+import {quasar, transformAssetUrls} from '@quasar/vite-plugin';
+import path from 'path';
+import {mergeConfig} from 'vite';
+//import * as hq from 'alias-hq';
+
+export default {
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {
+      docgen: 'vue-component-meta',
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  stories: ['../src/**/*.stories.ts',],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-links',
+    'storybook-addon-vue-slots',
+  ],
+  core: {
+    disableTelemetry: true,
+  },
+  viteFinal(config) {
+    config.plugins.push(...[
+      vue({
+        template: {transformAssetUrls},
+      }),
+      quasar({
+        sassVariables: 'src/css/quasar.variables.scss',
+      }),
+    ]);
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...{
+        '@': path.resolve(__dirname, '../src'),
+        'src': path.resolve(__dirname, '../src'),
+        'app': path.resolve(__dirname, '../src/components'),
+        'components': path.resolve(__dirname, '../src/components'),
+        'layouts': path.resolve(__dirname, '../src/layouts'),
+        'pages': path.resolve(__dirname, '../src/pages'),
+        'assets': path.resolve(__dirname, '../src/assets'),
+        'boot': path.resolve(__dirname, '../src/boot'),
+        'stores': path.resolve(__dirname, '../src/stores'),
+      },
+    };
+
+    return config;
+  },
+} as StorybookConfig;
---
+++ .storybook/preview.ts
@@ -0,0 +1,49 @@
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css';
+import 'quasar/dist/quasar.sass';
+import '@/css/app.scss';
+import {Preview, setup} from '@storybook/vue3';
+import {createPinia} from 'pinia';
+import {Quasar, useQuasar} from 'quasar';
+import quasarUserOptions from '../.quasar/dev-spa/quasar-user-options';
+
+setup(async (app) => {
+  app.use(Quasar, quasarUserOptions);
+  app.use(createPinia());
+
+  return Promise
+    .all([
+      import('src/boot/axios'),
+      import('src/boot/quasar'),
+    ])
+    .then(async (files) => {
+      const functions = files.map(file => file.default).filter(file => typeof file === 'function');
+      for (const func of functions) {
+        await func({app});
+      }
+    });
+});
+
+const preview: Preview = {
+  parameters: {
+    actions: {argTypesRegex: '^on[A-Z].*'},
+    controls: {
+      include: /^.*/,
+      expanded: true,
+      sort: 'requiredFirst',
+    },
+  },
+  decorators: [
+    (story) => ({
+      components: {story},
+      setup() {
+        const q = useQuasar();
+        q.dark.set(true);
+      },
+      template: '<story />',
+    }),
+  ],
+};
+
+export {
+  preview as default,
+};
