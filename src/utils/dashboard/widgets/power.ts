63c4dfe51d61d4a0dfed3e194b6d45435ab66c08
derpierre65 <git@derpierre65.de>
2025-03-16 11:17:06 +0000

    fix(widget): spelling mistake in power switch label

--- src/utils/dashboard/widgets/power.ts
+++ src/utils/dashboard/widgets/power.ts
@@ -15,7 +15,7 @@ export default function registerPowerWidgets() {
     configuration: [
       {
         id: 'switchId',
-        label: 'dashboard.widgets.power_switch_witcher.configuration.power_switch',
+        label: 'dashboard.widgets.power_switch_switcher.configuration.power_switch',
         required: true,
         type: 'select',
         fromEndpoint: 'getSwitches',
