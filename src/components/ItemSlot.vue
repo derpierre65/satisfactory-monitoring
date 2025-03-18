2d92788369e5b9ebff98b847a0ab1054a08c9394
derpierre65 <git@derpierre65.de>
2025-03-18 00:03:40 +0000

    feat: show fluids amount in slots in m³ (test)

--- src/components/ItemSlot.vue
+++ src/components/ItemSlot.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tw-relative tw-bg-neutral-300 tw-rounded-md tw-p-1" :style="{height: imageSize, width: imageSize}">
     <img :src="itemUrl" :alt="itemName">
-    <div class="absolute-bottom-right tw-bg-satisfactory-orange tw-text-white tw-rounded-md tw-px-1 text-weight-bold tw-text-base">
+    <div class="absolute-bottom-right tw-bg-satisfactory-orange tw-text-white tw-rounded-md tw-px-1 text-weight-bold">
       {{ itemAmount }}
     </div>
 
@@ -11,7 +11,7 @@
 
 <script setup lang="ts">
 import { InventoryItemObject } from '@derpierre65/ficsit-remote-monitoring';
-import useServerStore from 'stores/server.ts';
+import useServerStore from 'stores/server';
 import { computed } from 'vue';
 
 const {
@@ -30,6 +30,17 @@ const serverStore = useServerStore();
 
 const imageSize = computed(() => size + 'px');
 const itemName = computed(() => (typeof item === 'string' ? title : item.Name));
-const itemAmount = computed(() => (typeof item === 'string' ? amount : item.Amount));
+const itemAmount = computed(() => {
+  if (typeof item === 'string') {
+    return amount;
+  }
+
+  // TODO maybe replace with a list of fluids (not sure if 1000 works good here)
+  if (item.MaxAmount >= 1000) {
+    return (item.Amount / 1000).toFixed(1);
+  }
+
+  return item.Amount;
+});
 const itemUrl = computed(() => serverStore.getItemUrl(typeof item === 'string' ? item : item.ClassName));
 </script>
