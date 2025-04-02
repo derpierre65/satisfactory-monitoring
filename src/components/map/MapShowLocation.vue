42ed831eae20bcdca21dfc942cb738cf1742c82d
derpierre65 <git@derpierre65.de>
2025-04-02 19:11:52 +0100

    feat: add "show players" option to show players in "show location" dialog

--- src/components/map/MapShowLocation.vue
+++ src/components/map/MapShowLocation.vue
@@ -6,14 +6,18 @@
 
     <q-dialog v-model="showDialog">
       <q-card class="tw-w-full !tw-max-w-[60vw] tw-h-[80vh] shadow-0">
-        <q-card-section>
+        <q-card-section class="tw-flex">
           <q-img v-if="imgUrl" :src="imgUrl" width="32px" />
           <span>{{ title || entities[0].name }}</span>
           <span v-if="entities.length > 1"> ({{ entities.length }})</span>
+
+          <q-space />
+
+          <q-toggle v-model="playerInfo.isActive" label="Show Players" />
         </q-card-section>
         <SatisfactoryMap :center="entities[0].location" :zoom="-8">
           <MapMarker
-            v-for="entity in entities"
+            v-for="entity in allEntities"
             :key="entity.ID"
             :lat-lng="entity.location"
             :image="entity.image || ''"
@@ -26,9 +30,13 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, reactive, ref } from 'vue';
 import SatisfactoryMap from 'components/SatisfactoryMap.vue';
 import MapMarker from 'components/map/MapMarker.vue';
+import { usePausableFRMEndpoint } from 'src/composables/frmEndpoint';
+import { Endpoint, GetPlayerResponse } from '@derpierre65/ficsit-remote-monitoring';
+import { getEntityLocation } from 'src/utils/map';
+import useServerStore from 'stores/server';
 
 const props = withDefaults(defineProps<{
   entities: Array<{
@@ -45,7 +53,28 @@ const props = withDefaults(defineProps<{
   entities: () => [],
 });
 
+const serverStore = useServerStore();
+const playerInfo = reactive(usePausableFRMEndpoint<GetPlayerResponse>(Endpoint.GetPlayer, {
+  startPaused: true,
+  fetchDuration: 2_500,
+}));
+
 const showDialog = ref(false);
+
+const allEntities = computed(() => {
+  if (!playerInfo.isActive) {
+    return props.entities;
+  }
+
+  return props.entities.concat(playerInfo.data.map((player) => {
+    return {
+      ID: player.ID,
+      location: getEntityLocation(player),
+      name: player.Name,
+      image: serverStore.getItemUrl('Desc_Helmet_Default_C'),
+    };
+  }));
+});
 
 const imgUrl = computed(() => {
   if (props.image === null) {
